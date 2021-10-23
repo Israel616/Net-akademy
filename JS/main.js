993767ee5968489f9de14616507b909c99cbde05
AvFl <82409897+AvielFl@users.noreply.github.com>
2021-10-22 21:09:49 -0500

    Update

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,16 @@
 console.log('alfa version');
+//Do a function that can count numbers
+//Create a function that can count the number of words in a string
+//Create a function that can count the number of characters in a string
+function countNumbers(str) {
+    var count = 0;
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] >= '0' && str[i] <= '9') {
+            count++;
+        }
+    }
+    return count;
+}
+console.log(countNumbers('123456789'));
+//create a const that get a button by id
+var button = document.getElementById('button');
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-Hi
+<h1>Net Academy</h1>
+Este es un projecto creado con el proposito de alentar la enseñansa de la ciencia y la tecnología.
+<h2>Capturas</h2>
+<img src="" width='400px'>
+<img src="" width='400px'>
+<img src="" width='400px'>
+<h2>Creadores</h2>
+<ul>
+    <li>Aviel Axel Flores Melo</li>
+    <li>Aaron Israel Vazques Aguilar</li>
+</ul>
